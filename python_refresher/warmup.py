79f84ebf08116a64b6c6d9f0dab24fabe5500da7
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-12 11:37:52 -0400

    Warmup

---
+++ python_refresher/warmup.py
@@ -0,0 +1,16 @@
+# x = 1
+# y = 2.5
+# name = 'Michael'
+# is_cool = False
+
+#Multiple Assignment
+x,y,name,is_cool = (1,2.5,'Michael',False)
+
+print (x,y,name,is_cool)
+
+a = x + y
+print (a)
+
+print (type(x))
+x = str(x)
+print (type(x))
