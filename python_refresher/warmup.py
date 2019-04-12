760ff256b4f7ae18c9e088ea843f536d42dd70e4
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-12 11:50:24 -0400

    Warmup

--- python_refresher/warmup.py
+++ python_refresher/warmup.py
@@ -6,11 +6,18 @@
 #Multiple Assignment
 x,y,name,is_cool = (1,2.5,'Michael',False)
 
-print (x,y,name,is_cool)
+#print (x,y,name,is_cool)
 
 a = x + y
-print (a)
+#print (a)
 
-print (type(x))
+#print (type(x))
 x = str(x)
-print (type(x))
+#print (type(x))
+
+#arguments by position
+print('{2},{0},{1}'.format('a','b','c'))
+
+#print('I\'m {name} and I\'m cool: {is_cool}'.format(name=name,is_cool=is_cool))
+#F-Strings
+print(f'My name is {name}.')
