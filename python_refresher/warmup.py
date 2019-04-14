e95d8c6745b1a7b96c5333b767b56c23dd71890e
Michael Rubino <parkcathedralgrace@gmail.com>
2019-04-13 23:22:27 -0400

    Warmup

--- python_refresher/warmup.py
+++ python_refresher/warmup.py
@@ -16,8 +16,24 @@ x = str(x)
 #print (type(x))
 
 #arguments by position
-print('{2},{0},{1}'.format('a','b','c'))
+#print('{2},{0},{1}'.format('a','b','c'))
 
 #print('I\'m {name} and I\'m cool: {is_cool}'.format(name=name,is_cool=is_cool))
+
 #F-Strings
-print(f'My name is {name}.')
+#print(f'My name is {name}.')
+
+fruit_tuple = ('Apple','Banana','Orange')
+#fruit_tuple = tuple(('Apple','Banana','Orange'))
+#print (fruit_tuple[0])
+
+fruit_set = {'Apple','Banana','Orange'}
+#print(fruit_set)
+#print('Apple' in fruit_set)
+fruit_set.add('Grape')
+#print('Grape' in fruit_set)
+fruit_set.remove('Banana')
+#print('Banana' in fruit_set)
+fruit_set.clear()
+#del fruit_set
+print(fruit_set)
